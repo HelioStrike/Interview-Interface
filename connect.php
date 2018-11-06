883062fb21268cb15579d43f671595eca3e77f63
HelioStrike <rageofepicfury@gmail.com>
2018-11-06 20:38:22 +0530

    committed

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
+<!-- Establish conenction to sql -->
 <?php
 
 session_start();
 
-$servername = "ec2-18-206-54-177.compute-1.amazonaws.com";
-$username = "sandeep";
-$password = "sandeep123";
-$dbname = "interview_database";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "arp";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
