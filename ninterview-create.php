1e1af25b71409990ee6577b513074b54cc321ea7
HelioStrike <rageofepicfury@gmail.com>
2019-01-21 21:43:50 +0530

    committed

--- ninterview-create.php
+++ ninterview-create.php
@@ -9,29 +9,19 @@ $result = $conn->query($command);
 $row = $result->fetch_assoc();
 $icount = $row["interviews"];
 
-$shours = (ord($_POST["starttime"][0]) - ord('0'))*10 + ord($_POST["starttime"][1]) - ord('0');
-$smins = (ord($_POST["starttime"][3]) - ord('0'))*10 + ord($_POST["starttime"][4]) - ord('0');
-$ehours = (ord($_POST["endtime"][0]) - ord('0'))*10 + ord($_POST["endtime"][1]) - ord('0');
-$emins = (ord($_POST["endtime"][3]) - ord('0'))*10 + ord($_POST["endtime"][4]) - ord('0');
-
-if($_POST["studentid"] == '' || $_POST["description"] == '' || $_POST["qbankid"] == '' || strlen($_POST["starttime"]) != 5 ||
-     strlen($_POST["endtime"]) != 5 || shours > 23 || shours < 0 || smins < 0 || smins > 59 || 
-     ehours > 23 || ehours < 0 || emins < 0 || emins > 59)
-{
-    header("Location: /interview/ninterview.php");
-    exit();
-}
+$starttime = $_POST["startyear"].'-'.$_POST["startmonth"].'-'.$_POST["startday"].' '.$_POST["starthour"].':'.$_POST["startminute"].':00';
+$endtime = $_POST["endyear"].'-'.$_POST["endmonth"].'-'.$_POST["endday"].' '.$_POST["endhour"].':'.$_POST["endminute"].':00';
 
 $command = "UPDATE next_ids SET interviews=".($icount + 1)." WHERE interviews=".$icount;
 $conn->query($command);
 
 //insert new entry into interviews_master table
-$command = "INSERT INTO interviews_master(student_id, interview_id, description, bank_id, start_time, end_time, status, created_by) VALUES('" 
-    . $_POST["studentid"] . "'," . $icount . ",'" . $_POST["description"] . "'," . $_POST["qbankid"] . "," . ($shours*100+$smins) . "," . 
-    ($ehours*100+$emins) . ",'" . "active" . "','" . $_SESSION["username"] . "')";
+$command = "INSERT INTO interviews_master(student_id, interview_id, description, question_bank_id, start_time, end_time, status, created_by) VALUES('" 
+    . $_POST["studentid"] . "'," . $icount . ",'" . $_POST["description"] . "'," . $_POST["qbankid"] . ",'" . $starttime . "','" . 
+    $endtime . "','" . "active" . "','" . $_SESSION["username"] . "')";
 
 $conn->query($command);
 
-header("Location: /interview/interviews.php");
+header("Location: /interview/interviews.php?status=1111");
 
 ?>
